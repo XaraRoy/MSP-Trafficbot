a8f9888e7885f878fb47b4a1e52fab2c633a87dc
xanendorf <xanendorf@gmail.com>
2018-10-11 18:14:40 -0500

    Fixed brackets  in def()

--- send_email_with_attachments.py
+++ send_email_with_attachments.py
@@ -1,24 +1,23 @@
-##httpsgithub.comTeCoEdWhats-NewstreemasterCode
+##https://github.com/TeCoEd/Whats-News/tree/master/Code
 import smtplib,ssl
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
 from email.utils import formatdate
 from email import encoders
-# in the config.py file, we put the senders's gmail accountand&password in the following form
-#EMAIL_ADDRESS=  
-#PASSWORD=   
+# in the config.py file, we put the senders's gmail accountand&password in the following form:
+#EMAIL_ADDRESS=" " 
+#PASSWORD=" "  
 #from config  import EMAIL_ADDRESS,PASSWORD
 import os 
-EMAIL_ADDRESS= os.environ.get(EMAIL_ADDRESS)
-PASSWORD= os.environ.get(PASSWORD)
-# subject = sending email with attachments
+EMAIL_ADDRESS= os.environ.get("EMAIL_ADDRESS")
+PASSWORD= os.environ.get("PASSWORD")
+# subject = "sending email with attachments"
 # body = 'Hi there, we are sending this email from Python!'
 ### Function to send the email ###
-def send_an_email(file_name,subject=sending email with attachments,
-            body='from Python!')
-    #global subject,body
-    ##add xander and jeff's emails here
+def send_an_email(file_name,subject="sending email with attachments", body='from Python!'):
+    #global subject,bodybody
+    ##add xander and jeff's emails here:
 
     #toaddr_s = ['yjjiangphysics@gmail.com','Kreitzer.gr@gmail.com','xanendorf@gmail.com']
     toaddr_s = ['yjjiangphysics@gmail.com']
@@ -28,20 +27,21 @@ def send_an_email(file_name,subject=sending email with attachments,
     msg['Subject'] = subject
     msg['From'] = me
     msg['To'] = 'yjjiangphysics@gmail.com'
-    msg.preamble = test  
+    msg.preamble = "test " 
     msg.attach(MIMEText(body,'plain'))
 
-    part = MIMEBase('application', octet-stream)
+    part = MIMEBase('application', "octet-stream")
 
-    #put the attachments in the following, once in the open(),
-    #once in the filename=. The second  is only to ensure the 
-    #right file format
-    part.set_payload(open(file_name, rb).read())
+    #put the attachments in the following, once in the open(?),
+    #once in the filename=?. The second ? is only to ensure the 
+    #right file format:
+    part.set_payload(open(file_name, "rb").read())
     encoders.encode_base64(part)
-    part.add_header(f'Content-Disposition', f'attachment; filename={file_name}')
+    part.add_header(f'Content-Disposition', f'attachment; filename={file_name.split("/")[-1]}')
     msg.attach(part)
 
-    try
+
+    try:
        s = smtplib.SMTP('smtp.gmail.com', 587)
        s.ehlo()
        s.starttls()
@@ -51,15 +51,15 @@ def send_an_email(file_name,subject=sending email with attachments,
        aa=[s.sendmail(me, toaddr, msg.as_string()) for toaddr in toaddr_s]
        s.quit()
     
-    #except
-    #   print (Error unable to send email)
-#httpsstackoverflow.comquestions13957829how-to-use-raise-keyword-in-python
-#httpswww.youtube.comwatchv=b0PAVVchc7c
-    except Exception as e
+    #except:
+    #   print ("Error: unable to send email")
+#https://stackoverflow.com/questions/13957829/how-to-use-raise-keyword-in-python
+#https://www.youtube.com/watch?v=b0PAVVchc7c
+    except Exception as e:
           print (e)
-    finally
+    finally:
         pass
           
 
-# file=..try.html
+# file="..//try.html"
 # send_an_email(file)
